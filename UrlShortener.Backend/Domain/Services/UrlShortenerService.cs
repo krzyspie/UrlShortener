273fe307011919fdc7d2799723b93532892aa1fe
krzysiek <krzys_7@op.pl>
2023-11-15 13:10:38 +0100

    Create service to short url

---
+++ UrlShortener.Backend/Domain/Services/UrlShortenerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+
+namespace Domain.Services
+{
+    public class UrlShortenerService : IUrlShortenerService
+    {
+        public string ShortenUrl()
+        {
+            return string.Empty;
+        }
+    }
+}
