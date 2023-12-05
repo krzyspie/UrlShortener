aaf4175213a7270fda9bfb815e45c7b46fd9defd
krzysiek <krzys_7@op.pl>
2023-12-05 21:39:35 +0100

    Add query handler tests

---
+++ UrlShortener.Backend/UrlShortener.Backend.Tests/QueryHanlders/GetSourceUrlHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using Application.Queries;
+using Application.QueryHandlers;
+using Infrastructure.Repository;
+using NSubstitute;
+
+namespace Application.Tests.QueryHanlders
+{
+    public class GetSourceUrlHandlerTests
+    {
+        [Fact]
+        public async Task Should_ReturnSourceUrl_WhenShortcutExists()
+        {
+            // Arrange
+            var originalUrl = "Test";
+            var query = new GetSourceUrl()
+            {
+                ShortUrl = "test_url"
+            };
+
+            var urlRepositoryMock = Substitute.For<IUrlRepository>();
+            urlRepositoryMock.GetShortUrl(query.ShortUrl).Returns(originalUrl);
+
+            var handler = new GetSourceUrlHandler(urlRepositoryMock);
+
+            // Act
+            var result = await handler.Handle(query, default);
+
+            // Assert
+            Assert.Equal(originalUrl, result);
+
+            urlRepositoryMock.Received(1).GetShortUrl(query.ShortUrl);
+        }
+    }
+}
